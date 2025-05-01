9e9cf48db513c633f2c7089e30010b1de34eb804
Guerre-Pablo-Agustin <guerre.pablo.agustin@gmail.com>
2025-04-30 23:53:16 -0300

    feat: actualizar configuración de Tailwind y optimizar estilos globales

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
 import alpinejs from '@astrojs/alpinejs';
+import tailwind from '@astrojs/tailwind'; // Importación correcta para Tailwind v3
 
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
   integrations: [
+    tailwind(), // Integración oficial de Tailwind para Astro
     alpinejs()
   ]
 });
